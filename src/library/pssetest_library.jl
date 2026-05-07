34b76dc2be0588b2fef4e32777537db681d23117
m-bossart <bossart.matt@gmail.com>
2026-05-06 17:59:42 -0700

    add icd cases

--- src/library/pssetest_library.jl
+++ src/library/pssetest_library.jl
@@ -25,3 +25,11 @@ function build_pti_30(; raw_data, kwargs...)
     sys = PSY.System(PSY.PowerFlowDataNetwork(raw_data), sys_kwargs...)
     return sys
 end
+
+function build_psse_modified_14bus_sys(; raw_data, kwargs...)
+    sys = PSY.System(raw_data; runchecks = false)
+    # The RAW file defines two type-3 (REF) buses in the same island;
+    # demote bus 108 to PV so there is exactly one reference bus per subnetwork.
+    PSY.set_bustype!(PSY.get_component(PSY.ACBus, sys, "BUS 108"), PSY.ACBusTypes.PV)
+    return sys
+end
